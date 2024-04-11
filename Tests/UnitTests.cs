c699d96cd05cb7491087f8ad4e478872a40d19ef
EldinSalja <esalja2@huskers.unl.edu>
2024-04-11 16:33:27 -0500

    Commit correct unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -37,6 +37,7 @@ namespace GithubActionsLab
             Assert.AreEqual(2, Program.Subtract("4", "2"));
             Assert.AreEqual(1, Program.Subtract("5", "4"));
             Assert.AreEqual(-3, Program.Subtract("3", "6"));
+            Assert.AreEqual(-5, Program.Subtract("3", "6"));
         }
 
         [Test]
@@ -72,7 +73,9 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Multiply_NullNguyen()
+
+        public void Multiply_NullSalja()
+
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -80,7 +83,9 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Divide_ValidNguyen()
+
+        public void Divide_ValidSalja()
+
         {
             Assert.AreEqual(3, Program.Divide("6", "2"));
             Assert.AreEqual(5, Program.Divide("10", "2"));
@@ -88,7 +93,9 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Divide_InvalidNguyen()
+
+        public void Divide_InvalidSalja()
+
         {
             Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
@@ -96,7 +103,8 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Divide_NullNguyen()
+
+        public void Divide_NullSalja()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
